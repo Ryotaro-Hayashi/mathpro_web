a134d9b9a2687bbe38d5e1a9f1a0358922ac7378
hysrtr <hys.rtr.forit>
2019-11-17 03:15:02 +0900

    [add] ナビゲーションバーを揃えるのと、ルーティングを修正

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,7 @@
 class StaticPagesController < ApplicationController
+  def index
+    render layout: false
+  end
   def home
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root 'static_pages#home'
+  root 'static_pages#index'
+  get  '/home',       to: 'static_pages#home'
   get  '/concept',   to: 'static_pages#concept'
   get  '/usage',    to: 'static_pages#usage'
   get  '/frie',    to: 'static_pages#frie'
